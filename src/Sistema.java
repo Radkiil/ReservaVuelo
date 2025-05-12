588ba7aa7e9a2d1c2ea46ae8a8bcbb5830e7ec84
Radkiil <dfcastroc@udistrital.edu.co>
2025-05-12 18:16:30 -0500

    Vuelo

---
+++ src/Sistema.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Scanner;
+
+public class  Sistema {
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+        Vuelo v1 = new Vuelo();
+        Vuelo v2 = new Vuelo();
+        Vuelo v3 = new Vuelo();
+    }
+}
+
---
+++ src/Vuelo.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Vuelo {
+
+    Scanner leer = new Scanner(System.in);
+
+    public int id;
+    public String avion;
+    public int antiguedad;
+    public String origen;
+    public String destino;
+    public LocalDateTime fechaSalida;
+    public LocalDateTime fechaLlegada;
+}
+
