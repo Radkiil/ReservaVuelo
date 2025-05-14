20c07f5fab5f2f025ac7a63fb015d0348d0012de
Radkiil <dfcastroc@udistrital.edu.co>
2025-05-14 16:27:21 -0500

    Final

--- src/Pasajero.java
+++ src/Pasajero.java
@@ -8,11 +8,30 @@ public class Pasajero {
     Scanner leer = new Scanner(System.in);
 
     public String nombre;
-    public int id;
+    public int cc;
     public String nacionalidad;
     public String direccion;
     public int telefono;
     public String email;
     public boolean equipaje;
     public boolean pasaporte;
+
+    public Pasajero(){
+        System.out.println("Ingresar nombre");
+        nombre = leer.nextLine();
+        System.out.println("Ingresar documento");
+        cc = leer.nextInt();
+        System.out.println("Ingresar nacionalidad");
+        nacionalidad = leer.nextLine();
+        System.out.println("Ingresar direccion de residencia");
+        direccion = leer.nextLine();
+        System.out.println("Ingresar telefono");
+        telefono = leer.nextInt();
+        System.out.println("Ingresar email");
+        email= LocalDateTime(leer.nextLine());
+        System.out.println("Lleva equipaje?");
+        equipaje = LocalDateTime(leer.nextLine());
+        System.out.println("Tiene pasaporte?");
+        pasaporte = leer.nextLine();
+    }
 }
--- src/Reserva.java
+++ src/Reserva.java
@@ -15,5 +15,18 @@ public class Reserva {
     public boolean aprobado;
     public boolean checkIn;
 
-    idAsientos.lenght = asientosReservados;
+    public Reserva(){
+        System.out.println("Ingresar el numero de asientos que desea comprar");
+        asientosReservados = leer.nextInt();
+        System.out.println("Ingresar precio total");
+        precio = leer.nextInt();
+        System.out.println("Ingresar metodo de pago");
+        metodoDePago = leer.nextLine();
+        System.out.println("Ya realizo el check In");
+        checkIn = leer.nextInt();
+
+
+        idAsientos.lenght = asientosReservados;
+    }
+
 }
